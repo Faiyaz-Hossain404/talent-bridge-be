f2c4d77f7c9b72160127eb6844fb29bf55b7c460
Taofique <shornob8@gmail.com>
2025-11-07 22:49:46 +0600

    fix: Removed scripts folder and files, it was a test folder

--- src/scripts/pass.ts
+++
@@ -1,7 +0,0 @@
-import { hashPassword } from "../utils/bcrypt";
-
-(async () => {
-  const password = "admin123"; // your desired admin password
-  const hashed = await hashPassword(password);
-  console.log("Hashed password:", hashed);
-})();
